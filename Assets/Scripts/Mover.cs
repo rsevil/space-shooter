144cc71411e092a91e31d266279a81b010f6d6a9
rsevil <rodrigo.sevil@etermax.com>
2019-08-30 22:13:55 -0300

    creating shoots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿ using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody _rigidBody;
+    
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+    
+    void Start()
+    {
+        _rigidBody.velocity = transform.forward * speed;
+    }
+}
